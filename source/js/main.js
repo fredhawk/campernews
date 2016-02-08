555b567992337710cc134537c8304914b3f5374b
oeyece <fredrichawk@gmail.com>
2016-02-08 20:35:28 +0100

    Added comments

--- source/js/main.js
+++ source/js/main.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
   function newsArticles(newsItemsAPI) {
     $.get(newsItemsAPI, function(newsItem){
       newsItem.forEach(function(newsStory) {
+        
+        // Put all the needed values into variables for use.
         var newsImage = newsStory.image;
         var newsLink = newsStory.link;
         var newsTitle = newsStory.headline.substring(0, 25)+"...";
@@ -15,31 +17,30 @@ $(document).ready(function() {
         var newsUpvotes = newsStory.rank;
         var newsAuthorPic = newsStory.author.picture;
         
+        // If the article doesn't have an image, use the authors image instead.
         if (newsImage === "" || newsImage === "/web/imgs/logo.png") {
           newsImage = newsAuthorPic;
         }
         
+        // Format the date to "Mon Feb 08 2016"
         newsPostedDate = new Date(newsPostedDate);
         newsPostedDate = newsPostedDate.toDateString();
-        //console.log(newsImage + " " + newsLink + " " + newsTitle + " " + newsPoster + " " + newsPostedDate + " " + newsUpvotes + " " + newsAuthorPic)
-        console.log(newsImage)
         
+        // Create the newsarticle 
         html += '<article class="item">';
         html += '<a href="' + newsLink + '" target="_blank" class="item-link">';
-        html += '<img src ="' + newsImage + '" class="item-img">';
+        html += '<img src ="' + newsImage + '" class="item-img" alt="' +  + '">';
         html += '<h2 class="item-title">' + newsTitle + '</h2></a>';
         html += '<p class="post-date">Posted on: ' + newsPostedDate + '</p>';
-        html += '<a href="http://www.freecodecamp.com/' + newsPoster + '" class="author-link" target="_blank">';
+        html += '<a href="http://www.freecodecamp.com/' + newsPoster + '" class="author-link" target="_blank" alt="' + newsPoster + '">';
         html += '<p class="author">By ' + newsPoster + '</p></a>';
         html += '<div class="upvote">';
-        html += '<img src="./img/heart.svg" class="upvote-icon">';
+        html += '<img src="./img/heart.svg" class="upvote-icon" alt="Upvotes">';
         html += '<p class="upvotes">' + newsUpvotes + '</p></div></article>'
         
       });
-      // add html to .wrapper class
+      // Add html to .wrapper class for each newsarticle.
       $(".wrapper").html(html);
     });
   }
 });
-
-///Make the whole card a link!!!!!!!!!!!!!!!
